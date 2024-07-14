efb16eb1ceb7d78f8c4416140e7febf68c4fbc95
Xeostop1 <stop1@daum.net>
2024-07-14 12:58:28 +0900

    Your commit message here

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -7,22 +7,22 @@ export default async function Home() {
   const projects: Project[] = await getProjects();
 
   return (
-    <div className="flex flex-col items-center bg-black text-white min-h-screen pt-20">
+    <div className="flex flex-col items-center pt-20 bg-black text-white min-h-screen">
       <h1 className="text-4xl mb-8">Project 공통 소개</h1>
       <nav className="flex flex-col items-center gap-8">
         {projects.map((project) => (
-          <Link key={project.path} href={`/projects/${project.path}`} className="text-white no-underline w-full max-w-md">
-            <div className="bg-white bg-opacity-10 text-black rounded-lg p-6 text-center shadow-md backdrop-blur-lg mb-8">
+          <Link key={project.path} href={`/projects/${project.path}`} className="text-white no-underline">
+            <div className="bg-white bg-opacity-10 text-black rounded-lg p-6 text-center w-4/5 max-w-xl shadow-md backdrop-blur-lg border border-opacity-20">
               <Image
                 src={`/images/${project.path}.png`}
                 alt={project.title}
                 width={300}
                 height={200}
-                className="rounded-lg w-full h-auto mx-auto"
+                className="rounded-lg w-full h-auto"
               />
-              <h2 className="mt-4 text-xl text-white">{project.title}</h2>
-              <p className="text-gray-300"><strong>Date:</strong> {project.date}</p>
-              <p className="text-gray-300">{project.descript}</p>
+              <h2 className="mt-4 text-xl">{project.title}</h2>
+              <p><strong>Date:</strong> {project.date}</p>
+              <p>{project.descript}</p>
             </div>
           </Link>
         ))}
